f44643de09b31de1652a852f41e1c910dc07cbf5
Chittipobng014 <tpl_army@hotmail.com>
2020-03-21 21:06:46 +0700

    .

--- back-end/src/member/entities/member.entity.ts
+++ back-end/src/member/entities/member.entity.ts
@@ -12,32 +12,58 @@ export class Member extends AppBaseEntity {
     name: string;
 
     @Column({ nullable: true })
-    commission: number;
+    sReward: number
 
     @Column({ nullable: true })
-    sReward: number
+    sCommission: number;
 
     @Column({ nullable: true })
     a1Reward: number
 
     @Column({ nullable: true })
+    a1Commission: number;
+
+    @Column({ nullable: true })
     b1Reward: number
 
     @Column({ nullable: true })
+    b1Commission: number;
+
+    @Column({ nullable: true })
     c1Reward: number
+
+    @Column({ nullable: true })
+    c1Commission: number;
+
     @Column({ nullable: true })
     aReward: number
 
     @Column({ nullable: true })
+    aCommission: number;
+
+    @Column({ nullable: true })
     bReward: number
+
+    @Column({ nullable: true })
+    bCommission: number;
+
     @Column({ nullable: true })
     cReward: number
+
+    @Column({ nullable: true })
+    cCommission: number;
 
     @Column({ nullable: true })
     bigReward: number
 
     @Column({ nullable: true })
+    bigCommission: number;
+
+    @Column({ nullable: true })
     nReward: number
+
+    @Column({ nullable: true })
+    nCommission: number;
 
     @OneToMany(type => Purchase, purchase => purchase.member, {
         cascade: true,
@@ -45,19 +71,27 @@ export class Member extends AppBaseEntity {
     purchases: Purchase[];
 
     public static fromDTO(dto: CreateMemberDTO) {
-        const { name, commission, sReward, a1Reward, b1Reward, c1Reward, aReward, bReward, cReward, bigReward, nReward } = dto;
+        const { name, sReward, a1Reward, b1Reward, c1Reward, aReward, bReward, cReward, bigReward, nReward, a1Commission, aCommission, b1Commission, bCommission, bigCommission, c1Commission, cCommission, nCommission, sCommission } = dto;
         const member = new Member();
         member.name = name;
-        member.commission = commission;
         member.sReward = sReward;
+        member.sCommission = sCommission
         member.a1Reward = a1Reward;
+        member.a1Commission = a1Commission
         member.b1Reward = b1Reward;
+        member.b1Commission = b1Commission
         member.c1Reward = c1Reward;
+        member.c1Commission = c1Commission
         member.aReward = aReward;
+        member.aCommission = aCommission
         member.bReward = bReward;
+        member.bCommission = bCommission
         member.cReward = cReward;
+        member.cCommission = cCommission
         member.bigReward = bigReward;
+        member.bigCommission = bigCommission
         member.nReward = nReward
+        member.nCommission = nCommission
         return member;
     }
 }
---
+++ front-end/src/commons/member-list-table-columns.js
@@ -0,0 +1,134 @@
+export const memberListTableColumns = [{
+    name: "name",
+    label: "ชื่อ",
+    field: "name",
+    sortable: true,
+    align: `center`
+  },
+  {
+    name: "sReward",
+    label: "S",
+    field: "sReward",
+    align: `center`
+    // format: (val, row) => `บาทละ ${val}`
+  },
+  {
+    name: "sCommission",
+    label: "ค่าคอม S",
+    field: "sCommission",
+    align: `center`
+    // format: (val, row) => `${val}%`
+  },
+  {
+    name: "a1Reward",
+    label: "A1",
+    field: "a1Reward",
+    align: `center`
+    // format: (val, row) => `บาทละ ${val}`
+  },
+  {
+    name: "a1Commission",
+    label: "ค่าคอม A1",
+    field: "a1Commission",
+    align: `center`
+    // format: (val, row) => `${val}%`
+  },
+  {
+    name: "b1Reward",
+    label: "B1",
+    field: "b1Reward",
+    align: `center`
+    // format: (val, row) => `บาทละ ${val}`
+  },
+  {
+    name: "b1Commission",
+    label: "ค่าคอม B1",
+    field: "b1Commission",
+    align: `center`
+    // format: (val, row) => `${val}%`
+  },
+  {
+    name: "c1Reward",
+    label: "C1",
+    field: "c1Reward",
+    align: `center`
+    // format: (val, row) => `บาทละ ${val}`
+  },
+  {
+    name: "c1Commission",
+    label: "ค่าคอม S",
+    field: "c1Commission",
+    align: `center`
+    // format: (val, row) => `${val}%`
+  },
+  {
+    name: "aReward",
+    label: "A",
+    field: "aReward",
+    align: `center`
+    // format: (val, row) => `บาทละ ${val}`
+  },
+  {
+    name: "aCommission",
+    label: "ค่าคอม A",
+    field: "aCommission",
+    align: `center`
+    // format: (val, row) => `${val}%`
+  },
+  {
+    name: "bReward",
+    label: "B",
+    field: "bReward",
+    align: `center`
+    // format: (val, row) => `บาทละ ${val}`
+  },
+  {
+    name: "bCommission",
+    label: "ค่าคอม B",
+    field: "bCommission",
+    align: `center`
+    // format: (val, row) => `${val}%`
+  },
+  {
+    name: "cReward",
+    label: "C",
+    field: "cReward",
+    align: `center`
+    // format: (val, row) => `บาทละ ${val}`
+  },
+  {
+    name: "cCommission",
+    label: "ค่าคอม C",
+    field: "cCommission",
+    align: `center`
+    // format: (val, row) => `${val}%`
+  },
+  {
+    name: "bigReward",
+    label: "Big",
+    field: "bigReward",
+    align: `center`
+    // format: (val, row) => `บาทละ ${val}`
+  },
+  {
+    name: "bigCommission",
+    label: "ค่าคอม Big",
+    field: "bigCommission",
+    align: `center`
+    // format: (val, row) => `${val}%`
+  },
+  {
+    name: "nReward",
+    label: "N",
+    field: "nReward",
+    align: `center`
+    // format: (val, row) => `บาทละ ${val}`
+  },
+  {
+    name: "nCommission",
+    label: "ค่าคอม N",
+    field: "nCommission",
+    align: `center`
+    // format: (val, row) => `${val}%`
+  }
+]
